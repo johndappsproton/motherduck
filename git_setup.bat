b322e9d4639b8479d79fab1cce3c594b41971574
johndapps@gmail.com <johndapps@gmail.com>
2024-11-12 14:27:27 +0100

    Add/update files

---
+++ git_setup.bat
@@ -0,0 +1,18 @@
+rem git+_setup.bat
+rem
+set GIT_USER=johndappsproton
+
+rem Set the repository URL
+set REPO_URL=https://github.com/johndappsproton/motherduck.git
+
+rem Clone the repository (if it doesn't exist locally)
+rem if not exist Motherduck git clone %REPO_URL%
+
+rem Navigate to the repository directory
+rem cd Motherduck
+
+rem Add the files you want to upload
+rem git add .
+
+rem Commit the changes
+rem git commit -m "Add/update files"
